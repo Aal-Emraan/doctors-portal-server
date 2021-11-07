a85043179938decc66d6ba1b05f4365bceea4b2e
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-08 00:31:44 +0600

    database connected

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wd6nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connected succesfully');
+    }
+    finally{
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req,res) => {
     res.send('hello from server')
 })
