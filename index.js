41a614c7d36a6608ff8e84dcf7143b8dc59b9152
Aal Emraan <developer.aalemraan@gmail.com>
2021-11-08 21:04:18 +0600

    updated

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { MongoClient } = require('mongodb');
 
 // middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wd6nd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,10 +16,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        console.log('database connected succesfully');
+        console.log('server connected succesfully');
+
+        const database = client.db("doctorsPortal");
+        const appointmentsCollection = database.collection("allAppointments");
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            console.log(date);
+            const query = {email: email};
+            const allAppointments = appointmentsCollection.find(query);
+            const appointments = await allAppointments.toArray();
+            res.json(appointments)
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const bookingInfo = req.body;
+            console.log(bookingInfo);
+            const result = await appointmentsCollection.insertOne(bookingInfo);
+            // console.log(result);
+            res.send(result);
+        })
     }
     finally{
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
